8d16a018540c86cfc408c47f4329998715a2473d
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-13 13:42:48 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from nextcord.ext import commands
+from nextcord import Intents
+bot = commands.Bot(command_prefix=["?"], help_command=None, intents=Intents.all())
+from os import listdir, environ, system
+for i in listdir('cogs'): bot.load_extension(f'cogs.{i[: -3]}') if i.endswith('.py') else print('not a python file')
+@bot.event
+async def on_message(message):
+  if message.guild.id == 996596265389404241:
+    await bot.process_commands(message)
+bot.run(environ['token'])
