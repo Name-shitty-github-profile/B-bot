520fe83e03af2b9eb8fa9b694da12f53daca45b5
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-13 16:29:01 -0400

    Update invite.py

--- cogs/invite.py
+++ cogs/invite.py
@@ -4,8 +4,12 @@ class Invite(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
+  @commands.command()
+  async def invite(self, ctx):
+    await ctx.send("__B Community__\n**Un serveur professionnel, de pub et de chill**\n\n**Des règlements simples**\n\n**Des urls custom pour les partenaires**\n\n**Des chats actifs**\n\n**Des staff actifs**\n\n**Des salons pubs à volontée**\nhttps://discord.gg/9ME3mppG7z")
+
   @commands.Cog.listener("on_message")
-  async def invite(self, message):
+  async def censor(self, message):
     if 'staff' not in [role.name for role in message.author.roles] and message.guild.id == 996596265389404241:
       if message.channel.id not in autorised_channel or message.channel.name.startswith("ticket"):
         if any(word in message.content.lower() for word in ['discord.gg/']) and len(message.content) > 12:
