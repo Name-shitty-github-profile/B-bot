23f35b89ce3354305c1b9a494019e58b728344a4
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-13 16:14:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from nextcord.ext import commands
 from nextcord import Intents
 bot = commands.Bot(command_prefix=["?"], help_command=None, intents=Intents.all())
 from os import listdir, environ, system
-for i in listdir('cogs'): bot.load_extension(f'cogs.{i[: -3]}') if i.endswith('.py') else print('not a python file')
+for fn in listdir('cogs'): bot.load_extension(f'cogs.{fn[: -3]}') if i.endswith('.py') else print('not a python file')
 @bot.event
 async def on_message(message):
   if message.guild.id == 996596265389404241:
